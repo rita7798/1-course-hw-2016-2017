3f57273cdba1d054c8b03555df00b0b78e7169eb
Margarita Berseneva <rita7798@gmail.com>
2016-10-17 22:54:15 +0300

    fixed 2.0

--- hw3/3 и 4 задача вместе.py
+++ hw3/3 и 4 задача вместе.py
@@ -1,17 +1,12 @@
-print ("3. Даёт ли a остаток c при делении на b?")
 a = int(input ("a = "))
 b = int(input ("b = "))
 c = int(input("c = "))
+print ("3. Даёт ли a остаток c при делении на b?")
 if a % b == c:
     print('Да')
 else:
     print('Нет')
-
-
 print ("4. Является ли c решением линейного уравнения ax + b = 0?")
-a = int(input ("a = "))
-b = int(input ("b = "))
-c = int(input("c = "))
 if a * c + b == 0:
     print('Да')
 else:

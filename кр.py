ccf3c0007652d7385443701275f0c38b2fc1a3ec
Margarita Berseneva <rita7798@gmail.com>
2016-12-06 15:04:11 +0300

    new

--- кр.py
+++ кр.py
@@ -7,4 +7,18 @@ for line in lines:
   if len(words) < 10: 
     print(line[0], ' — ', line[1])
 
-    
+
+with open('quotes.txt','r', encoding = 'utf-8') as f: 
+  text = f.read() 
+  lines = text.split('\n') 
+count = 0 
+authors = [] 
+for line in lines: 
+  line = line.split(' — '); 
+  if "разум" in line[0]: 
+      count += 1 
+      authors.append(line[1]) 
+print("со словом 'разум' нашлось", count, "цитат ") 
+for author in authors[:-1]: 
+  print(author, end=", ") 
+print(authors[-1])

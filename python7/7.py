0173a680a72cb605633fffd3c5cb16cdd53fa3b6
Margarita Berseneva <rita7798@gmail.com>
2017-01-12 23:51:56 +0300

    un

--- python7/7.py
+++ python7/7.py
@@ -1,36 +1,36 @@
-def opening():
+#coding = utf-8
+
+def opening(name):
     words = []
-    with open (input('Введите имя файла: '), 'r', encoding='utf-8') as f:
-        for line in f.read().split():
+    with open (name, 'r', encoding='utf-8') as f:
+        for line in f.read().replace('\n', ' ').split():
             words.append(line.lower())
     for i in range(len(words)):
         words[i] = words[i].strip('.,!?*()«»\'";:-')
     return words
 
-def appending_uns():
-    words = opening()
+def counting(words):
     uns = []
+    count = 0
     for i in range(len(words)):
         if words[i].startswith("un"):
             uns.append(words[i])
-    return uns
-    
-def counting():
-    count = 0
-    uns = appending_uns()
-    for i in range(len(uns)):
-        count += 1                
+            count += 1
     print("С приставкой -un в тексте нашлось", count, "слов(а).")
     return uns
     
-def comparison():
-    bigger = []
-    uns = counting()
-    for i in range(len(uns)):
-        if len(uns[i]) > n:
-            bigger.append(uns[i])
-    percent = int(len(bigger) / len(uns) * 100)
-    print("Длинну большую,чем", n, ", имеет", percent, "% слов.")
-    
-n = int(input("Введите число: "))
-comparison()
+def comparison(uns):
+    if len(uns) != 0:
+        print("----------------------------------------------------")
+        n = int(input("Введите число: "))
+        bigger = []
+        for i in range(len(uns)):
+            if len(uns[i]) > n:
+                bigger.append(uns[i])
+        percent = int(len(bigger) / len(uns) * 100)
+        print("Длинну большую,чем", n, ", имеет", percent, "% слов.")
+
+def main():
+    comparison(counting(opening(input('Введите имя файла: '))))
+
+main()

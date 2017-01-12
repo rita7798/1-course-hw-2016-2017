558deee82dfe26b02f4a4c4b1086d02aa0b1c51d
Margarita Berseneva <rita7798@gmail.com>
2017-01-12 23:51:12 +0300

    ness

--- python7/2.py
+++ python7/2.py
@@ -1,14 +1,15 @@
-def opening():
+#coding = utf-8
+
+def opening(name):
     words = []
-    with open (input('Введите имя файла: '), 'r', encoding='utf-8') as f:
-        for line in f.read().split():
+    with open (name, 'r', encoding='utf-8') as f:
+        for line in f.read().replace('\n', ' ').split():
             words.append(line.lower())
     for i in range(len(words)):
         words[i] = words[i].strip('.,!?*()«»\'";:-')
     return words
 
-def counting():
-    words = opening()
+def counting(words):
     count = 0
     prev = 0
     a = 0
@@ -25,10 +26,13 @@ def counting():
         a = ness.count(word)
         if a >= prev:
             prev = a
-            max_word = word
-        
+            max_word = word       
     print("В данном тексте", count, "разных существительных с суффиксом -ness.")
+    print("----------------------------------------------------------")
     if count != 0:
         print("Существительное", max_word, "имеет максимальную частотность - найдено", prev, "слов(а).")   
 
-counting()
+def main():
+    counting(opening(input('Введите имя файла: ')))
+
+main()

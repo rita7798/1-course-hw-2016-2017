bf43a3e236196c7fdada477c6855f46d04082dc1
Margarita Berseneva <rita7798@gmail.com>
2017-01-12 23:51:21 +0300

    hood

--- python7/3.py
+++ python7/3.py
@@ -1,14 +1,15 @@
-def opening():
+#coding = utf-8
+
+def opening(name):
     words = []
-    with open (input('Введите имя файла: '), 'r', encoding='utf-8') as f:
-        for line in f.read().split():
+    with open (name, 'r', encoding='utf-8') as f:
+        for line in f.read().replace('\n', ' ').split():
             words.append(line.lower())
     for i in range(len(words)):
         words[i] = words[i].strip('.,!?*()«»\'";:-')
     return words
 
-def counting():
-    words = opening()
+def counting(words):
     count = 0
     prev = 1000
     a = 0
@@ -31,19 +32,26 @@ def counting():
         if a <= prev:
             prev = a
             min_word = word
-        
+            # min_word - слово, число вхождений которого минимально.
+            # Если таких слов несколько,то вывестись может любое из них. 
     print("В данном тексте", count, "существительных с суффиксом -hood.")
-    print("Существительное", min_word, "имеет минимальную частотность - найдено всего", prev, "слов(а).")   
+    print("----------------------------------------------------")
+    if count != 0:
+        print("Существительное", min_word, "имеет минимальную частотность - найдено всего", prev, "слов(о).")   
     return hoods_samples
 
-def forming():
+def forming(hoods_samples):
     forms = []
-    hoods_samples = counting()
     for word in hoods_samples:
         word = word[:-4]
         forms.append(word)
-    print("Корни существительных с суффиксом -hood:")
-    for i in range(len(forms)):
-        print(forms[i], sep = "\n")
+    if len(forms) != 0:
+        print("----------------------------------------------------")
+        print("Корни существительных с суффиксом -hood:")
+        for i in range(len(forms)):
+            print(forms[i], sep = "\n")
 
-forming()
+def main():
+    forming(counting(opening(input('Введите имя файла: '))))
+
+main()    

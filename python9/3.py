fbc590a1104df1ea30d2921b0c7560a0b3b1ecb3
Margarita Berseneva <rita7798@gmail.com>
2017-01-30 21:01:51 +0300

    progat'

--- python9/3.py
+++ python9/3.py
@@ -12,11 +12,13 @@ def searching(fname):
     words = set()
     for i in range (len(texte)-1):
         texte[i] = texte[i].strip('”“".,«»\\/*!:;—()\'-?')
-        prog = re.match('программир(у(ют?|е(шь|те?|м(ы(ми|м|й)|ой|ых|ые|ом(у)?|а(я)?|ы|ого|ый|ую|ое|))|я)|о(ва(л[аи]?|ть(ся)?|нн?[аы]?(й|ого|ому|ую|м(и)?|ом|ое|ой|я|е|х|))|))', texte[i])
+        prog = re.match('программир(ова(ть.*|л[аои]?|в|нн.*|)|у(й(.*)?|я(сь)?|е.*|ю.*))', texte[i])
         if prog != None:
             words.add(texte[i])
-        prog2 = re.match('буд(у(чи|т)?|е(шь|те?|м))', texte[i])
-        if prog2 != None:
+        prog2 = re.match('буд(у(.*)?|е.*)', texte[i])
+        prog = re.match('программир(ова(ть.*|л[аои]?|в|нн.*|)|у(й(.*)?|я(сь)?|е.*|ю.*))', texte[i+1])
+        if prog2 != None and prog != None:
+            texte[i+1] = texte[i+1].strip('”“".,«»\\/*!:;—()\'-?')
             word = texte[i] +" "+ texte[i+1]
             word = str(word)
             words.add(word)
@@ -24,7 +26,7 @@ def searching(fname):
         print("Формы глагола 'программировать' в файле: ")
         for word in words:
             print (word)
-        print("Всего найдено", len(words), "штук(и)." )
+        print("Всего разных форм найдено", len(words), "штук(и)." )
     else:
         print("Ни одной формы глагола 'программировать' в файле не найдено." )
 

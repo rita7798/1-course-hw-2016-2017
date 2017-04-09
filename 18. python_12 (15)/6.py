562a7930b800f4e66ff9d59f5d811810debbf8c2
Margarita Berseneva <rita7798@gmail.com>
2017-04-09 20:51:02 +0300

    sentence со словами длины 1.

---
+++ 18. python_12 (15)/6.py
@@ -0,0 +1,33 @@
+#coding = utf-8
+
+import re
+
+
+def opening_text(fname):
+    sentences = []
+    words = []
+    with open (fname, 'r', encoding = 'utf-8') as f:
+        text = f.read()
+    new_text = re.sub('[\.\?\!…]', 'q', text)
+    sentences = new_text.split('q')
+    for i in range(len(sentences)):
+        sentences[i] = re.sub('[\.,*<>«»:;\'\"\n–-]','', sentences[i])
+        sentences[i] = re.sub('[\xa0]',' ', sentences[i]) #особенность моего текста
+        sentences[i] = sentences[i].strip()
+    return (sentences)
+
+
+def making_list(sentences):
+    true_sentences = [[sentence, round((len(sentence) - (len(sentence.split(' ')) - 1)) / (len(sentence.split(' '))), 1)] for sentence in sentences if len(sentence.split(' ')) > 10 ]
+    template = "Предложение \"{}\" со словами длинны {}"
+    for i in true_sentences:
+        print(template.format(i[0], i[1]))
+    
+
+def main():
+    sentences = opening_text(input("Введите название файла: "))
+    making_list(sentences)
+
+
+main()
+

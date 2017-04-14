0de200027c38db9b151917b39e467a214fea51db
Margarita Berseneva <rita7798@gmail.com>
2017-04-14 23:52:50 +0300

    files with puncts

---
+++ 19. python_13 (16)/6.py
@@ -0,0 +1,33 @@
+#coding = utf-8
+
+
+import os
+import re
+
+files = 0
+files_names = []
+path = os.getcwd()
+fileslist = [file for file in os.listdir() if os.path.isfile(file)]
+
+names = []
+for file in fileslist:
+    name = file.rsplit('.', maxsplit=1)[0]
+    names.append(name)
+
+printing_names = []
+true_names = [name for name in names if re.search('[.,!?:;)(-]', name) != None]
+for i in range(len(true_names)):
+    files += 1
+    if true_names[i] not in printing_names:
+        printing_names.append(true_names[i])
+
+print("Найдено ", files, " файлов(ла): ")
+for i in range(len(printing_names)):
+    print(printing_names[i])
+    
+
+        
+    
+
+
+
